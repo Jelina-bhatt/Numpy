6ea4f590b1ab3514038d277e1d549d858e137227
Jelina <jelinabhatt1@gmail.com>
2025-09-30 22:11:33 +0545

    array attributes

---
+++ Array_attribute2.py
@@ -0,0 +1,9 @@
+#for 2D array
+import numpy as np
+arr2d = np.array([[1, 2, 3],
+                  [4, 5, 6]])
+
+print("Array:\n", arr2d)
+print("Shape:", arr2d.shape)   # (2, 3) → 2 rows, 3 cols
+print("Dimensions:", arr2d.ndim) # 2D
+print("Size:", arr2d.size)     # 6 elements

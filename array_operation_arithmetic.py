25cc9fc28cbe1c714225edc55a7140107c6aa58d
Jelina <jelinabhatt1@gmail.com>
2025-10-01 22:55:23 +0545

    arithmetic

---
+++ array_operation_arithmetic.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+arr1 = np.array([1, 2, 3])
+arr2 = np.array([4, 5, 6])
+
+print("Addition:", arr1 + arr2)   # [5 7 9]
+print("Subtraction:", arr1 - arr2) # [-3 -3 -3]
+print("Multiplication:", arr1 * arr2) # [4 10 18]
+print("Division:", arr2 / arr1)  # [4.  2.5 2. ]

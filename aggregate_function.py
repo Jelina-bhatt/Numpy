6dd787a58aa333c40df5519ea56816ac73baeef8
Jelina <jelinabhatt1@gmail.com>
2025-10-01 22:56:41 +0545

    aggregate function

---
+++ aggregate_function.py
@@ -0,0 +1,8 @@
+import numpy as np
+arr = np.array([10, 20, 30, 40, 50])
+
+print("Sum:", arr.sum())     # 150
+print("Mean:", arr.mean())   # 30.0
+print("Max:", arr.max())     # 50
+print("Min:", arr.min())     # 10
+print("Standard Deviation:", arr.std()) # 14.14...
